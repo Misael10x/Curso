77556f2c029d420043f5a635fdaa717d6bf18477
Misael Barbosa <misabar12389@gmail.com.com>
2024-09-02 20:08:47 -0600

    mensaje de commit

---
+++ Tipo_Datos.py
@@ -0,0 +1,9 @@
+## String
+
+nombre = "Misael"
+apellido = "Barbosa"
+nombreCompleto =  "Hola " + nombre + " " + apellido
+#print(nombre) ## va sin comillas simples ni dobles ya que en este caso solo queremos mostrar lo que hay.
+#print("Hola " + nombre)
+#print(type(nombre)) #imprime el tipo de dato de la variable
+print(nombreCompleto)
