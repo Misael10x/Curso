daec919873764cd651fab61758b8370e9f711cb8
Misael Barbosa <misabar12389@gmail.com.com>
2024-09-03 20:37:41 -0600

    mensaje de commit

---
+++ IF.py
@@ -0,0 +1,12 @@
+# Instrucciones IF
+# es un bloque de codigo que se ejecuta si su condicion es verdadera
+
+edad = int(input("¿Cuantos años tienes? "))
+
+if edad ==100:
+    print("Tienes un siglo de vida")
+elif edad >=18:
+    print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")
+    
