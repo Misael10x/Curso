125e2ff5fc0a9895800c4b87a5381872371958d1
Misael Barbosa <misabar12389@gmail.com.com>
2024-09-03 20:39:05 -0600

    mensaje de commit

--- IF.py
+++ IF.py
@@ -3,10 +3,10 @@
 
 edad = int(input("¿Cuantos años tienes? "))
 
-if edad ==100:
+if edad ==100:   # primero se comprieba si tiene 100 años y si es asi
     print("Tienes un siglo de vida")
-elif edad >=18:
+elif edad >=18: # solicita si eres mayor de edad o menor de edad
     print("Eres mayor de edad")
-else:
+else: #si no se cumple saldra esto
     print("Eres menor de edad")
     
