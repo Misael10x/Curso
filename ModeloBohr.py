63d5c11365f227418862f4c89bb668e732b2a676
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-04-05 22:05:38 -0600

    Update ModeloBohr.py

--- ModeloBohr.py
+++ ModeloBohr.py
@@ -1,3 +1,4 @@
 def energia_nivel_hidrogeno():
     n = int(input("Introduce el número cuántico principal (n ≥ 1): "))
     if n < 1:
+        print("\nEl número cuántico debe ser mayor o igual a 1.")
